fdb32652dd0304856f0353c171f6a8c94d1f157d
Marcelino Álvarez García <marcelinoalvarezgarcia@gmail.com>
2023-04-28 10:16:41 +0200

    OrderedVector<Integer>

--- src/test/java/uoc/ds/pr/util/OrderedVector.java
+++ src/test/java/uoc/ds/pr/util/OrderedVector.java
@@ -1,31 +1,108 @@
 package uoc.ds.pr.util;
 
+import edu.uoc.ds.adt.sequential.FiniteContainer;
 import edu.uoc.ds.traversal.Iterator;
+import edu.uoc.ds.traversal.IteratorArrayImpl;
+import edu.uoc.ds.traversal.IteratorTraversalImpl;
 
 import java.util.Comparator;
 
-public class OrderedVector<T> {
-    public OrderedVector(T len, Comparator<T> cmp) {
+public class OrderedVector<Integer> implements FiniteContainer<Integer> {
+
+    private Comparator<Integer> comparator;
+    private Integer[] data;
+    private int len;
+
+    public OrderedVector(int max, Comparator<Integer> comparator) {
+        this.comparator = comparator;
+        this.data= (Integer[])new Object[max];
+        this.len=0;
+    }
+
+    public Integer elementAt(int i) {
+        return this.data[i];
+    }
+
+    public void rshift(int i) {
+        int p=this.len-1;
+        while (p>=i) {
+            this.data[p+1]=this.data[p];
+            p--;
+        }
+    }
+
+    public void lshift(int i) {
+        int p=i;
+        while (p<this.len) {
+            this.data[p]=this.data[p+1];
+            p++;
+        }
+    }
+
+    public void update(Integer vIn) {
+        int i = 0;
+        boolean end=false;
+        Integer v = null;
+
+        this.delete(vIn);
+
+        if (this.isFull()) {
+            Integer pOut = this.last();
+            if (comparator.compare(pOut, vIn)>0) {
+                this.delete(pOut);
+                this.update(vIn);
+            }
+        }
+
+        while (i<this.len && this.data[i]!=null && this.comparator.compare(this.data[i], vIn)>=0)
+            i++;
+
+        rshift(i);
+
+        this.data[i]=vIn;
+        this.len++;
 
     }
 
-    public void update(T i) {
+    private Integer last() {
+        return this.last();
     }
 
+
+    @Override
+    public boolean isEmpty() {
+        return this.len==0;
+    }
 
     public int size() {
         return 0;
     }
 
+    @Override
+    public Iterator<Integer> values() {
+        return (Iterator<Integer>)new IteratorArrayImpl<Integer>(this.data, this.len,0);
+    }
+
     public boolean isFull() {
-        return false;
+        return this.len==this.data.length;
     }
 
-    public void delete(T i) {
+    public int numElements(){
+        return this.len;
     }
 
-    public Iterator<T> values() {
-        return null;
+    public void delete (Integer elem) {
+        int i=0;
+        boolean found=false;
+
+        while (!found && i<this.len)
+            found= (comparator.compare(elem, this.data[i++])>=0);
+
+        if (found) {
+            lshift(i-1);
+            this.len--;
+        }
+
     }
 
 
